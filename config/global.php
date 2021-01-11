1fdcfec89befd04909b37c66117c9ab719917ed4
naylinn <neylynn97@gmail.com>
2021-01-11 09:23:03 +0630

    update global

--- config/global.php
+++ config/global.php
@@ -5,19 +5,13 @@ return [
 	"STATUS_INACTIVE" => 0,
 	"PRODUCT_UPLOAD" => 16,
 	"CATEGORY_UPLOAD" => 17,
-<<<<<<< HEAD
 	"NEW_UPLOAD" => 18,
+	"COMPANY_UPLOAD" => 19,
 	"MEDIA_PATH" => [
 		16 => 'upload/product',
 		17 => 'upload/category',
-		18 => 'upload/news'
-=======
-	"COMPANY_UPLOAD" => 18,
-	"MEDIA_PATH" => [
-		16 => 'upload/product',
-		17 => 'upload/category',
-		18 => 'upload/company',
->>>>>>> 4497d1d1d9ba0a1b029f68ea72818b86e22ef65f
+		18 => 'upload/news',
+		19 => 'upload/company'
 	],
 	"MEDIA_TYPE" => [
 		'image' => array('field_name' => 'image_media', 'extension' => array("jpg", "png", "jpeg"), 'max_size' => 50000000000)
