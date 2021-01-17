2aa3e12b15f6fac1b4105410ba0928dffa11cd3c
Thura <mgaungthurawin@gmail.com>
2021-01-17 17:56:24 +0630

    update search result

--- resources/views/frontend/search_result.blade.php
+++ resources/views/frontend/search_result.blade.php
@@ -6,23 +6,20 @@
         <div class="row">
             <div class="col-lg-12 mb-12">
                 <div class="container-box" style="width: 100%;">
-                    @if(MATERIAL == $company_type->type)
+                    @if(MATERIAL == $category->prefix)
                       <h1>{{trans('app.db_material')}}</h1><br>
-                    @elseif(TEXTILE == $company_type->type)
+                    @elseif(TEXTILE == $category->prefix)
                       <h1>{{trans('app.db_textile')}}</h1><br>
                     @else
                       <h1>{{trans('app.db_food')}}</h1><br>
                     @endif
-                  <p>{{trans('app.search_result')}}</p>
-                  <p>{{trans('app.products_name')}}<br>
-                  {{trans('app.state_region')}}</p>
                 </div>
             </div>
-            {{--<div class="col-lg-6 mb-6">
+            <div class="col-lg-6 mb-6">
                 <div class="container-box" style="width: 100%;">
-                    <h3>Search Result</h3>
+                    <h3>{{trans('app.search_result')}}</h3>
                 </div>
-            </div>--}}
+            </div>
         </div>
 
         <div class="row">
