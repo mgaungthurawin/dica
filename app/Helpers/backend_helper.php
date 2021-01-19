466d5e8df4524859fe2de97c24398aa03fcf231d
mgaungthurawin <mgaungthurawin@gmail.com>
2021-01-19 14:14:43 +0630

    update word count

--- app/Helpers/backend_helper.php
+++ app/Helpers/backend_helper.php
@@ -236,6 +236,17 @@ function main_location($company) {
 }
 
 
+function limit_text($text, $limit) {
+    if (str_word_count($text, 0) > $limit) {
+        $words = str_word_count($text, 2);
+        $pos   = array_keys($words);
+        $text  = substr($text, 0, $pos[$limit]) . '...';
+    }
+    return $text;
+}
+
+
+
 
 
 
