9e0873f7c2cbee897f5a62b822af336ccc42d041
Psyche <mayhtoo18@gmail.com>
2021-01-22 14:28:38 +0630

    css changes

--- resources/views/frontend/layouts/css.blade.php
+++ resources/views/frontend/layouts/css.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="{{ asset('v2/css/bootstrap.min.css') }}" />
+<!-- <link rel="stylesheet" type="text/css" href="{{ asset('v2/css/bootstrap.min.css') }}" /> -->
 <link rel="shortcut icon" href="v2/images/moi_logo.png" sizes="130x130">
 <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 <link rel="stylesheet" type="text/css" href="{{ asset('v2/css/assetlibs.css') }}" />
