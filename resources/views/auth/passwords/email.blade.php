c5e8db07b49e0fab6d895dff77e0609ea95b8a0f
naylinn <neylynn97@gmail.com>
2021-01-25 13:27:55 +0630

    update ui in pasword reset

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,146 +1,35 @@
-{{--
-@extends('layouts.app')
-
+@extends('frontend.layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<main class="page_main_wrapper login text-centr register">            
+    <div class="container">    
+        <div class="row justify-content-center">
+            <div class="col-md-8 login-register-container" id="container">
 
-                <div class="card-body">
+                <h3 class="text-center">{{ __('Reset Password') }}</h3>
+                <div class="col-md-5">
+                <img src="{{ asset('v2/images/Artboard – 15.png') }}" alt="logo" width="100%"><br>
+                </div>
+                <div class="col-md-7">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
+                        @csrf                          
+                        <input id="email" type="email" placeholder="Email"
+                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+                           value="{{ old('email') }}" required autofocus>
+                            @if ($errors->has('email'))
+                                <span class="text-danger">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        <button class="btn btn-primary outline-btn" type="submit">{{ __('Send Password Reset Link') }}</button>
                     </form>
                 </div>
             </div>
-        </div>
+        <!-- /.row -->         
     </div>
-</div>
+</main>
 @endsection
---}}
-
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Matching Service Program</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
-    <link rel="stylesheet" href="{{ url('bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ url('bower_components/font-awesome/css/font-awesome.min.css')}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ url('bower_components/Ionicons/css/ionicons.min.css')}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ url('bower_components/admin-lte/dist/css/AdminLTE.min.css')}}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{ url('bower_components/admin-lte/plugins/iCheck/square/blue.css')}}">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <!-- Application CSS -->
-<!-- <link href="{{ asset("/css/custom.css")}}" rel="stylesheet" type="text/css"/> -->
-
-    <!-- Google Font -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    
-    <div class="login-logo">
-        <b>Matching Service Program</b>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <center><h4>{{ __('Reset Password') }}</h4></center><br>
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-            <div class="form-group has-feedback">
-                <input id="email" type="email" placeholder="Email"
-                       class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
-                       value="{{ old('email') }}" required autofocus>
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-                @endif
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-            </div>
-            
-            <div class="row">
-                <div class="col-xs-8">
-                    <div class="checkbox icheck">
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="form-group">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            {{ __('Send Password Reset Link') }}
-                        </button>
-                    </div>
-                </div>
-                <!-- /.col -->
-            </div>
-        </form>
-
-    </div>
-    <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery 3 -->
-<script src="{{ url('bower_components/jquery/dist/jquery.min.js')}}"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="{{ url('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-<!-- iCheck -->
-<script src="{{ url('bower_components/admin-lte/plugins/iCheck/icheck.min.js')}}"></script>
-<script>
-    $(function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' /* optional */
-        });
-    });
-</script>
-</body>
-</html>
